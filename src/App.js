90387b3e945c84ad0141182ab15e0608d30eeb35
DolapoLawal <daminat76@yahoo.com>
2023-07-05 18:36:59 +0100

    reactjsx

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Price from './Price';
+import Description from './Description';
+import Image from './Image';
+import Name from './Name';
+import {Card} from "react-bootstrap";
+import "./styles.css";
 
-function App() {
+const App = () => {
+  const firstName = "DOLAPO";
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <Card>
+      <Card.Body>
+        <Card.Title>
+          <Name />
+        </Card.Title>
+          <Card.Text>
+            <Price />
+            <Description />
+            <Image />
+          </Card.Text>
+      </Card.Body>
+    </Card>
+      <p className="greeting-text"> HELLO, {firstName ? firstName : "there"}!</p>
     </div>
   );
-}
-
+};
+  
 export default App;
---
+++ src/Product.js
@@ -0,0 +1,8 @@
+const Product = {
+        name : "Laptop",
+        price : "₦20,000",
+        description : "Laptop is a good gadget that help  people in researching ",
+        image : "https://www-konga-com-res.cloudinary.com/w_700,f_auto,fl_lossy,dpr_1.0,q_auto/media/catalog/product/I/U/177353_1664850422.jpg"
+};
+
+export default Product;
